9765d6a72395f4bb220767a079ea8f8beef80e34
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-30 19:58:04 +0700

    Update Doublelinkedlist.cpp

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -196,4 +196,23 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.reverse();
+            break;
+        case '5':
+            list.searchData();
+            break;
+        case '6':
+            return 0;
         
