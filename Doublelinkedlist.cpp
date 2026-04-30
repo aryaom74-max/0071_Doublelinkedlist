f41ff4a5effa66910d114de444c3731161eabe78
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-30 19:58:54 +0700

    Update Doublelinkedlist.cpp

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -219,4 +219,11 @@ int main()
             cout << "Invalid option\n";
         }
 
-        
+        cout << "\nPress enter to continue...";
+        cin.ignore();
+        cin.get();
+
+    } while (choice != '6');
+
+    return 0;
+}
